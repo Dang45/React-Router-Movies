8c00a73c6fac6e2c42be527c090f15914a60c830
spark45 <dgagz45@hotmail.com>
2021-07-11 23:06:27 -0400

    complete

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { useHistory, Link, useLocation } from 'react-router-dom';
 
 export default function MovieList(props) {
+  const { pathname } = useLocation()
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
+        <Link to={{pathname: `/movies/${movie.id}`}}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
-
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
   return (
     <div className="movie-card">
       <h2>{title}</h2>
